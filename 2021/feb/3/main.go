a7635924c0e8b42ef2e667c7f52939b7b490e501
Chad England <chad.england@accesscfa.com>
2021-02-03 13:49:43 -0500

    feb3-2021

--- 2021/feb/3/main.go
+++ 2021/feb/3/main.go
@@ -7,25 +7,24 @@ func main() {
 }
 
 /**
- * Definition for a binary tree node.
-  * type TreeNode struct {
+ * Definition for singly-linked list.
+  * type ListNode struct {
 	   *     Val int
-	    *     Left *TreeNode
-		 *     Right *TreeNode
-		  * }
+	    *     Next *ListNode
+		 * }
 */
+func hasCycle(head *ListNode) bool {
+	if head == nil {
+		return false
+	}
+	l1, l2 := head, head
 
-func trimBST(root *TreeNode, low int, high int) *TreeNode {
-	if root == nil {
-		return root
+	for l2 != nil && l2.Next != nil {
+		l2 = l2.Next.Next
+		l1 = l1.Next
+		if l1 == l2 {
+			return true
+		}
 	}
-	if root.Val > high {
-		return trimBST(root.Left, low, high)
-	}
-	if root.Val < low {
-		return trimBST(root.Right, low, high)
-	}
-	root.Left = trimBST(root.Left, low, high)
-	root.Right = trimBST(root.Right, low, high)
-	return root
+	return false
 }
